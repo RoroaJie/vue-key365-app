54348d95a0d3fc878b4cb4147b5ce4520ddcca8d
RoroaJie <602703774@qq.com>
2019-08-02 14:12:11 +0800

    基础配置

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-module.exports = {
-    devServer: {
-      proxy: ' http://localhost:8081/'
-    }
-  }
+// module.exports = {
+//     devServer: {
+//       proxy: ' http://localhost:8090'
+//     }
+//   }
